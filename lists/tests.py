1f84c5c9d4df6a9a45c0e8420d945c1a93ce3664
root <root@educacion.educacion>
2019-01-17 09:06:20 -0300

    User input testing with POST request

--- lists/tests.py
+++ lists/tests.py
@@ -16,6 +16,9 @@ class HomePageTest(TestCase):
         html = response.content.decode('utf8')
         self.assertTrue(html.strip().endswith('</html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
