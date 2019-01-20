7199b775cdd4e51327e60eefa0461580f350de9d
root <root@educacion.educacion>
2019-01-20 13:11:30 -0300

    A URL and View for New List Creation

--- lists/tests.py
+++ lists/tests.py
@@ -15,48 +15,47 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTemplateUsed(response, 'home.html')
 
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'lists.html')
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'lists.html')
+
+
+class NewListTest(TestCase):
+
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-    class ItemModelTest(TestCase):
-
-        def test_saving_and_retrieving_items(self):
-            first_item = Item()
-            first_item.text = 'The first (ever) list item'
-            first_item.save()
-
-            second_item = Item()
-            second_item.text = 'Item the second'
-            second_item.save()
-
-            saved_items = Item.objects.all()
-            self.assertEqual(saved_items.count(), 2)
-
-            first_saved_item = saved_items[0]
-            second_saved_item = saved_items[1]
-            self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-            self.assertEqual(second_saved_item.text, 'Item the second')
-
-    class ListViewTest(TestCase):
-
-        def test_displays_all_items(self):
-            Item.objects.create(text='itemey 1')
-            Item.objects.create(text='itemey 2')
-
-            response = self.client.get('/lists/the-only-list-in-the-world/')
-
-            self.assertContains(response, 'itemey 1')
-            self.assertContains(response, 'itemey 2')
-
-        def test_uses_list_template(self):
-            response = self.client.get('/lists/the-only-list-in-the-world/')
-            self.assertTemplateUsed(response, 'list.html')
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
